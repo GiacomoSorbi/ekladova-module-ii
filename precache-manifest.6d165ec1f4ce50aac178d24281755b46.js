7605c9e2647608b2e8763ebc5a7070aa34fa4b7d
Kate Kladova <ek.kladova@gmail.com>
2020-07-11 13:19:43 +0100

    Updates

---
+++ precache-manifest.6d165ec1f4ce50aac178d24281755b46.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c631c26a4bf6d54ec6dba413a0cae487",
+    "url": "/ekladova-module-ii/index.html"
+  },
+  {
+    "revision": "a052e58db25da0fd9486",
+    "url": "/ekladova-module-ii/static/css/main.27f2347e.chunk.css"
+  },
+  {
+    "revision": "c74dff158f340b472530",
+    "url": "/ekladova-module-ii/static/js/2.bfac331c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ekladova-module-ii/static/js/2.bfac331c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a052e58db25da0fd9486",
+    "url": "/ekladova-module-ii/static/js/main.62643ccb.chunk.js"
+  },
+  {
+    "revision": "b48c04269f629f74a3df",
+    "url": "/ekladova-module-ii/static/js/runtime-main.819a4485.js"
+  },
+  {
+    "revision": "60c279f53e918b90176a50b07bc9e7a6",
+    "url": "/ekladova-module-ii/static/media/award.60c279f5.png"
+  },
+  {
+    "revision": "aae5e9c3374920f734cafe841a7d763a",
+    "url": "/ekladova-module-ii/static/media/baby.aae5e9c3.png"
+  },
+  {
+    "revision": "8757b81956530c5e0f62b2cc8bc89214",
+    "url": "/ekladova-module-ii/static/media/banner-noname.8757b819.jpg"
+  },
+  {
+    "revision": "c19c7c2b38055cac37d6b9ba29feea85",
+    "url": "/ekladova-module-ii/static/media/bannertree.c19c7c2b.jpg"
+  },
+  {
+    "revision": "f6c7ae1746dc56de3b05bb7d9c16909b",
+    "url": "/ekladova-module-ii/static/media/book.f6c7ae17.png"
+  },
+  {
+    "revision": "e2529e87ef8026f5efd36aa69a5bfd81",
+    "url": "/ekladova-module-ii/static/media/pen.e2529e87.jpg"
+  },
+  {
+    "revision": "f9badf58ab25b5e6d6f9696b66c1c485",
+    "url": "/ekladova-module-ii/static/media/stroke.f9badf58.svg"
+  },
+  {
+    "revision": "5b24259a484c669ca7d85c0deee27f31",
+    "url": "/ekladova-module-ii/static/media/stroke3.5b24259a.svg"
+  },
+  {
+    "revision": "bf1a760d095139b44bc8255859e19134",
+    "url": "/ekladova-module-ii/static/media/timeline.bf1a760d.svg"
+  },
+  {
+    "revision": "3ac4965643f05986c1cdbe0362c943c6",
+    "url": "/ekladova-module-ii/static/media/woman.3ac49656.jpg"
+  },
+  {
+    "revision": "859fe552f61f9ff0351e24e06b0384fb",
+    "url": "/ekladova-module-ii/static/media/woman.859fe552.png"
+  }
+]);
